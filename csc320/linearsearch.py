aa646ed9303cdb9dcc36dd0f6bc472b7a8571aa4
wowbob396 <wowbob396@gmail.com>
2018-01-16 20:02:12 -0600

    created code for first two problems on assignment #1

---
+++ csc320/linearsearch.py
@@ -0,0 +1,6 @@
+def linearsearch(a,v):
+    n = len(a)
+    for i in range(n):
+        print(i)
+
+linearsearch([1,5,4,3,2],3)
---
+++ csc320/selectionSort.py
@@ -0,0 +1,17 @@
+def selectionSort(A):
+    n = len(A)
+    
+    for i in range(n - 1):
+        minVal = A[i]
+        print(A)
+        for j in range(i,n):
+            if(A[j] < minVal):
+                minVal = A[j]
+                minLoc = j
+        if A[i] != minVal:
+            temp = A[i]
+            A[i] = minVal
+            A[minLoc] = temp
+        
+
+s = selectionSort([4, 2, 1, 5,3,9,8])
