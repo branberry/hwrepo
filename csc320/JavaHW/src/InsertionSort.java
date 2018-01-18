bfa946e3bc3094e28b83a853b335928151f1cd5c
Brandon <wowbob396@gmail.com>
2018-01-18 12:05:18 -0600

    restructured project file for javaHW

--- csc320/JavaHW/src/InsertionSort.java
+++ csc320/JavaHW/src/InsertionSort.java
@@ -7,8 +7,8 @@ public class InsertionSort {
     public static int[] createRandomIntArray( int size) {
         int[] answer = new int[ size];
         Random generator = new Random();
-        for( int element : answer) {
-            element = generator.nextInt(100) + 1;
+        for( int i = 0; i < size;  i++) {
+            answer[i] = generator.nextInt(100) + 1;
         }
         return answer;
     }
@@ -26,7 +26,11 @@ public class InsertionSort {
     }
 
     public static void main(String[] args) {
-        int[] A = createRandomIntArray(10);
-
+        int[] A = new int[10];
+        A = createRandomIntArray(10);
+        sort(A);
+        for(int element : A) {
+            System.out.println(element);
+        }
     }
 }
