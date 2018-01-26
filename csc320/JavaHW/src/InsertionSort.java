b8891c90acf851d933014d86974ce3eb7fb906c2
wowbob396 <wowbob396@gmail.com>
2018-01-26 11:09:13 -0600

    refactored analysis code

--- csc320/JavaHW/src/InsertionSort.java
+++ csc320/JavaHW/src/InsertionSort.java
@@ -32,40 +32,24 @@ public class InsertionSort {
     }
 
     public static void main(String[] args) {
-        int[] A = new int[10];
-        int[] B = new int[100];
-        int[] C = new int[1000];
-        int[] D = new int[10000];
-        int[] E = new int[100000];
-        int[] F = new int[1000000];
+        /*
+            Initializing test arrays of varying sizes to sort with insertion sort up to n = 100000
+        */ 
+        int[][] Arrays = new int[5][];
+        float[] avg = new float[5];
 
-        float avgA = 0;
-        float avgB = 0;
-        float avgC = 0;
-        float avgD = 0;
-        float avgE = 0;
-        float avgF = 0;
-
-        for(int i = 0; i < 10; i++) {
-            A = createRandomIntArray(10);
-            B = createRandomIntArray(100);
-            C = createRandomIntArray(1000);
-            D = createRandomIntArray(10000);
-            E = createRandomIntArray(100000);
-
-            avgA += sort(A);
-            avgB += sort(B);
-            avgC += sort(C);
-            avgD += sort(D);
-            avgE += sort(E);
-
+        int j = 1;
+        for(int i = 0; i < Arrays.length; i++) {
+            j *= 10;
+            Arrays[i] = new int[j];
+            Arrays[i] = createRandomIntArray(j);
+            avg[i] += sort(Arrays[i]);
+            System.out.println(j);
         }
-        avgA = avgA/10;
-        avgB = avgB/10;
-        avgC = avgC/10;
-        avgD = avgD/10;
-        avgE = avgE/10;
 
-        System.out.println("Average number of operations when n = 10: " + avgA + " Average when n = 100: " + avgB + " Average number of operations when n = 1000: " + avgC + " Average when n = 10000: " + avgD + " Average when n = 100000: " + avgE);
+        for(int i = 0; i < avg.length; i++) {
+            avg[i] = avg[i]/10;
+            System.out.println("Size of the array: " + Arrays[i].length + " Estimated number of operations: " + avg[i]);
+        }
     }
 }
