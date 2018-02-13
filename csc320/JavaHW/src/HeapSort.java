9e7eac92848591a814d7f8084c52355639f0117b
Your Name <wowbob396@gmail.com>
2018-02-13 17:36:52 -0600

    created HeapSort for java

---
+++ csc320/JavaHW/src/HeapSort.java
@@ -0,0 +1,96 @@
+import java.lang.*;
+import java.util.Random;
+
+/**
+ *  @author Brandon Ly - CSC 320A
+ * 
+ *  This class is an implementation of Heap Sort from CLRS
+ *  This implementation will sort arrays of integers
+ */
+public class HeapSort {
+    /**
+     * 
+     */
+    public static int[] createRandomIntArray(int size) {
+        int[] answer = new int[size+1];
+        Random generator = new Random();
+        answer[0] = 0;
+        for( int i = 1; i < size; i++) {
+            answer[i] = generator.nextInt(100) + 1;
+        }
+        return answer;
+    }
+
+    public static int parent(int i) {
+        return (int) Math.floor(i/2);
+    }
+
+    public static int left(int i) {
+        return i * 2;
+    }
+
+    public static int right(int i) {
+        return i * 2 + 1;
+    }
+
+    /**
+     * swaps two values in the array
+     * @param A is the array that will have its value swapped
+     * @param i is the first index
+     * @param j is the second index
+     */
+    public static void swap(int[] A, int i, int j) {
+        int temp;
+        temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
+    public static void maxHeapify(int[] A, int i) {
+        int l = left(i);
+        int r = right(i);
+        int largest;
+
+        if(l < A.length && A[l] > A[i]) {
+            largest = l;
+        } else {
+            largest = i;
+        }
+
+        if(r < A.length && A[r] > A[largest]) {
+            largest = r;
+        }
+
+        if(largest != i) {
+            swap(A,i,largest);
+            maxHeapify(A,largest);
+        }
+    }
+
+    public static void buildMaxHeap(int[] A) {
+        for(int i = (int) Math.floor(A.length/2); i >= 1; i--) {
+            maxHeapify(A,i);
+        }
+    }
+
+    public static void sort(int[] A) {
+        buildMaxHeap(A);
+        int n = A.length - 1;
+        for(int i = n; i >= 2; i--) {
+            swap(A,1,i);
+            maxHeapify(A,1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] test = new int[11];
+        test = createRandomIntArray(10);
+        sort(test);
+
+        for(int i = 1; i < 11; i++) {
+            System.out.println(test[i]);
+        }
+    }
+}
+
+
